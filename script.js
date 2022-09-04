e3b9d30353037676870ed860dfb365559b8d3196
My Name <mdarfinji45@gmail.com>
2022-09-04 20:41:41 +0800

    product done

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ for (const product of data ){
 
 
 
- if(uniqueArray.indexOf(product.category) == -1){
+ if(uniqueArray.indexOf(product.category) === -1){
     uniqueArray.push(product.category);
     const li = document.createElement('li');
 
@@ -39,3 +39,41 @@ for (const product of data ){
 
 setAllMenu() 
 
+
+const  searchfield =document.getElementById('search-field');
+
+searchfield.addEventListener('keypress', async(Event) => {
+   
+    if(Event.key === 'Enter'){
+     
+const searchvalue = searchfield.value;
+
+const allProduct = await loadAllProducts()
+
+const FoundProducts = allProduct.filter(product => product.category.includes(searchvalue));
+
+const productcontainer = document.getElementById('products-container')
+
+FoundProducts.forEach(product => {
+   
+    const div =document.createElement('div')
+
+    div.innerHTML= `
+    <div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    
+    `
+    productcontainer.appendChild(div)
+})
+
+
+    }
+})
