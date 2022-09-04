c2fabcdea06f9e0b11634f3a43a031168146eddb
My Name <mdarfinji45@gmail.com>
2022-09-04 19:11:42 +0800

    start search

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ const loadAllProducts = async() => {
     const data =await response.json();
 
     return data;
-    /* here data is returned so i can use it anywhere i want  */
+    /* here data is returned so i can use it anywhere i want  async await return data but fetch dont  */
 }
+
+const setAllMenu = async () =>{
+    // console.log(loadAllProducts) here you will not get the data bcz promise is not resolved . below  the promise is resolved 
+    // loadAllProducts()
+    // .then(data => console.log(data))
+
+    const data = await loadAllProducts();
+
+    
+
+
+}
+
+setAllMenu() 
+
+setAllMenu();
